55078c4fa689192643761f9777ddebb6941ffb96
Rumeysa Akcanca <94036242+rumeysa-akcanca@users.noreply.github.com>
2022-08-29 17:16:13 +0300

    add .

---
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class CustomResponseDto<T>
+    {
+        //Api'larda döneceğiz
+        //Api'lar için best practise 
+
+        //endpointlerden geriye tek bir model dönsün ,işlem başarılı da olsa olmasa da 
+        public T Data { get; set; }
+        [JsonIgnore]//json'a dönüştürürkem Statuscode client'a dönmeyecek
+        public int StatusCode { get; set; }
+
+        public List<String> Errors { get; set; }
+
+        public static CustomResponseDto<T> Success(int statusCode,T data)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Data = data, Errors= null };
+        }
+
+        public static CustomResponseDto<T> Success(int statusCode)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode};
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode,Errors =errors };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, string error)
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { error} };
+        }
+    }
+}
